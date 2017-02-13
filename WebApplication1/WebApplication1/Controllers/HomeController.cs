e463e600b111147e7d860394f4ab66ba8e386ecc
Scott Sauber <stsauber@gmail.com>
2017-02-12 22:42:40 -0600

    Initial Commit

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MySettings _mySettings;
+
+        public HomeController(MySettings mySettings)
+        {
+            _mySettings = mySettings;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_mySettings);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
